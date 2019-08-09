15fa38df2d41d04ddee53e4843a0cda8ee48fdb4
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2019-08-09 20:14:04 +0530

    Update lanes.py

--- lanes.py
+++ lanes.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 #import matplotlib.pyplot as plt
+
+def coor(image,para):
+    m,c=para
+    y1=image.shape[0]
+    y2=int(y1*(3/5))
+    x1=int((y1-c)/m)
+    x2=int((y2-c)/m)
+    return np.array([x1,y1,x2,y2])
 image=cv2.imread('test_image.jpg')
 copy=np.copy(image)
 gray=cv2.cvtColor(copy,cv2.COLOR_RGB2GRAY)
@@ -20,6 +28,28 @@ if lines is not None:
     for line in lines:
         x1,y1,x2,y2=line.reshape(4)
         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-final_image=cv2.addWeighted(image,0.8,line_image,1,1)
-cv2.imshow("result",final_image)
+#final_image=cv2.addWeighted(image,0.8,line_image,1,1)
+left_fit=[]
+right_fit=[]
+for line in lines:
+    x1,y1,x2,y2=line.reshape(4)
+    para=np.polyfit((x1,x2),(y1,y2),1)
+    m=para[0]
+    c=para[1]
+    if m<0:
+        left_fit.append((m,c))
+    else:
+        right_fit.append((m,c))
+left_fit_average=np.average(left_fit,axis=0)
+right_fit_average=np.average(right_fit,axis=0)
+left_line=coor(image,left_fit_average)
+right_line=coor(image,right_fit_average)
+new_line=np.array([left_line,right_line])
+new_line_image=np.zeros_like(image)
+if new_line is not None:
+    for line in new_line:
+        x1,y1,x2,y2=line.reshape(4)
+        cv2.line(new_line_image,(x1,y1),(x2,y2),(255,0,0),10)
+new_image=cv2.addWeighted(image,0.8,new_line_image,1,1)
+cv2.imshow("result",new_image)
 cv2.waitKey(0)
